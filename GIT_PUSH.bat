c1e8d3a069e14dbede1237c11e079cb3dc123527
bagavievbulat <buba1219@yandex.ru>
2023-11-29 20:22:38 +0300

    Собесы 2023

--- GIT_PUSH.bat
+++ GIT_PUSH.bat
@@ -1,5 +1,5 @@
 chcp 65001
 git add .
-git commit -m "Да, было много плохого, много я сделал говна. Когда судьба наливает, то ты выпиваешь до дна."
+git commit -m "Собесы 2023"
 git push origin master
 pause
